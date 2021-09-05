e7e461122d83ded1c2c05c44d301cae30530ee1d
EC2 Default User <ec2-user@ip-172-31-42-183.us-east-2.compute.internal>
2021-09-05 03:33:58 +0000

    不具合修正

--- main.js
+++ main.js
@@ -8,9 +8,10 @@
     
     let startTime;
     let timeoutId;
+    let elapsedTime = 0;
     
     function countUp() {
-        const d = new Date (Date.now() - startTime);
+        const d = new Date (Date.now() - startTime + elapsedTime);
         const m = String(d.getMinutes()).padStart(2, '0');
         const s = String(d.getSeconds()).padStart(2, '0');
         const ms = String(d.getMilliseconds()).slice(0,-2);
@@ -21,16 +22,40 @@
         }, 10);
     }
     
+    function setButtonStartInitial() {
+        start.disabled = false;
+        stop.disabled = true;
+        reset.disabled = true;
+    }
+
+    function setButtonStartRunning() {
+        start.disabled = true;
+        start.disabled = true;
+        stop.disabled = false;
+    }
+    
+    function setButtonStartStopped() {
+        start.disabled = false;
+        stop.disabled = true;
+        reset.disabled = false;
+    }
+    
+    setButtonStartInitial()
+    
     start.addEventListener('mousedown', () => {
+        setButtonStartRunning()
         startTime = Date.now();
         countUp();
     });
     
     stop.addEventListener('mousedown', () => {
+        setButtonStartStopped()
         clearTimeout(timeoutId)
+        elapsedTime += Date.now() - startTime;
     });
     
     reset.addEventListener('mousedown', () => {
+        setButtonStartInitial()
         timer.textContent = '00:00.0'
     });
   }
